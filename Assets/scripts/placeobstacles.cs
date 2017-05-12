99da44535d1fd059add47ae265630cb2213032fd
root <root@DESKTOP-9SANUQD.localdomain>
2017-05-12 16:11:42 +0100

    added resources folder

---
+++ Assets/scripts/placeobstacles.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class placeobstacles : MonoBehaviour {
+    private GameObject startPoint;
+    private Vector3 lastLocation;
+    public List<Object> Obstacles;
+	// Use this for initialization
+	void Start () {
+        Obstacles = Resources.LoadAll("obstacles/").ToList();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
